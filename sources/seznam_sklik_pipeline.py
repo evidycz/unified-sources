5b22cc5b337166ead60cc2175ab0f8b5f54989c5
pleskop <peter.plesko@gmail.com>
2025-06-04 10:08:22 +0200

    feat: make access_token optional and update project metadata (#2)
    Allow `access_token` to be optional in `load_settings` and `load_stats` functions. Update project description and version in `pyproject.toml`.
    
    Co-authored-by: pleskop <peter@evidy.cz>

--- sources/seznam_sklik_pipeline.py
+++ sources/seznam_sklik_pipeline.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 import dlt
 
 from seznam_sklik import unified_sklik_source, unified_sklik_stat_source
 
 
-def load_settings(access_token: str) -> None:
+def load_settings(access_token: Optional[str] = None) -> None:
     pipeline = dlt.pipeline(
         pipeline_name="seznam_sklik_settings",
         destination="duckdb",
@@ -13,7 +15,8 @@ def load_settings(access_token: str) -> None:
     load_info = pipeline.run(setting_source)
     print(load_info)
 
-def load_stats(access_token: str) -> None:
+
+def load_stats(access_token: Optional[str] = None) -> None:
     pipeline = dlt.pipeline(
         pipeline_name="seznam_sklik_stats",
         destination="duckdb",
@@ -25,7 +28,5 @@ def load_stats(access_token: str) -> None:
 
 
 if __name__ == "__main__":
-    access_token = "0xff1206bea62c411ecd0e46beb485b279f464aa9b69d2080a799af0766daba26599e25"
-
-    # load_settings(access_token)
-    load_stats(access_token)
+    load_settings()
+    load_stats()
